8be26f20d72b71cb4b875979809a1c78a7a93a53
Louise Rodrigues <nutrilouiserodrigues@gmail.com>
2022-07-24 14:37:43 -0300

    Tratando erros com excecao personalizada

---
+++ xadrez-console1/Tabuleiro/TabuleiroException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Tabuleiro
+{
+    class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base(msg)
+        {
+
+        }
+    }
+
+}
+
