a01754a2f7b80589f5f41cdf409204651fce070e
Louise Rodrigues <nutrilouiserodrigues@gmail.com>
2022-07-27 14:48:26 -0300

    Jogada especial en passant

--- xadrez-console1/xadrez/Peao.cs
+++ xadrez-console1/xadrez/Peao.cs
@@ -10,8 +10,10 @@ namespace xadrez
 {
     internal class Peao : Peca
     {
-        public Peao(Tabuleiro.Tabuleiro tab, Cor cor) : base(tab, cor)
+        private PartidaDeXadrez partida;
+        public Peao(Tabuleiro.Tabuleiro tab, Cor cor, PartidaDeXadrez partida) : base(tab, cor)
         {
+            this.partida = partida;
         }
         public override string ToString()
         {
@@ -55,33 +57,94 @@ namespace xadrez
                 {
                     mat[pos.linha, pos.coluna] = true;
                 }
-                else
+
+                // #jogadaespecial en passant 
+                if (posicao.linha == 3)
                 {
-                    pos.definirValores(posicao.linha + 1, posicao.coluna);
-                    if (tab.posicaoValida(pos) && livre(pos))
+                    Posicao esquerda = new Posicao(posicao.linha, posicao.coluna - 1);
+                    if (tab.posicaoValida(esquerda) && existeInimigos(esquerda) && tab.peca(esquerda) == partida.vulneravelEnPassant)
                     {
-                        mat[pos.linha, pos.coluna] = true;
-                    }
-                    pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
-                    if (tab.posicaoValida(pos) && livre(pos) && existeInimigos(pos))
-                    {
-                        mat[pos.linha, pos.coluna] = true;
-                    }
-                    pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
-                    if (tab.posicaoValida(pos) && livre(pos) && qtdeMovimentos == 0)
-                    {
-                        mat[pos.linha, pos.coluna] = true;
+                        {
+                            mat[esquerda.linha, esquerda.coluna] = true;
+                        }
+
+                        // #jogadaespecial en passant 
+                        if (posicao.linha == 3)
+                        {
+                            Posicao direita = new Posicao(posicao.linha, posicao.coluna - 1);
+                            if (tab.posicaoValida(direita) && existeInimigos(direita) && tab.peca(direita) == partida.vulneravelEnPassant)
+                            {
+                                {
+                                    mat[direita.linha, direita.coluna] = true;
+                                }
+
+
+                            }
+                            else
+                            {
+                                pos.definirValores(posicao.linha + 1, posicao.coluna);
+                                if (tab.posicaoValida(pos) && livre(pos))
+                                {
+                                    mat[pos.linha, pos.coluna] = true;
+                                }
+                                pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
+                                if (tab.posicaoValida(pos) && livre(pos) && existeInimigos(pos))
+                                {
+                                    mat[pos.linha, pos.coluna] = true;
+                                }
+                                pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
+                                if (tab.posicaoValida(pos) && livre(pos) && qtdeMovimentos == 0)
+                                {
+                                    mat[pos.linha, pos.coluna] = true;
+                                }
+                            }
+                            // #jogadaespecial en passant 
+                            if (posicao.linha == 4)
+                            {
+                                esquerda = new Posicao(posicao.linha, posicao.coluna - 1);
+                                if (tab.posicaoValida(esquerda) && existeInimigos(esquerda) && tab.peca(esquerda) == partida.vulneravelEnPassant)
+                                {
+                                    {
+                                        mat[esquerda.linha, esquerda.coluna] = true;
+                                    }
+
+                                    // #jogadaespecial en passant 
+                                    if (posicao.linha == 3)
+                                    {
+                                        direita = new Posicao(posicao.linha, posicao.coluna - 1);
+                                        if (tab.posicaoValida(direita) && existeInimigos(direita) && tab.peca(direita) == partida.vulneravelEnPassant)
+                                        {
+                                            {
+                                                mat[direita.linha, direita.coluna] = true;
+                                            }
+
+
+
+
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
+
             return mat;
-
-
         }
-
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
